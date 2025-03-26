4a4082ec619614fd2b55ec8936d9aa3f8ef430de
katanujoy <katanujoyy99@gmail.com>
2025-03-26 13:57:10 +0300

    completed context lab

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
 }
 
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employee, dateStamp) {
+    try {
+        const [date, hour] = dateStamp.split(' ');
+        if (!date || !hour) throw new Error();
+        
+        employee.timeInEvents.push({
+            type: "TimeIn",
+            hour: parseInt(hour, 10),
+            date
+        });
+        return employee;
+    } catch(e) {
+        throw new Error("Invalid dateStamp provided");
+    }
+}
+
+function createTimeOutEvent(employee, dateStamp) {
+    try {
+        const [date, hour] = dateStamp.split(' ');
+        if (!date || !hour) throw new Error();
+        
+        employee.timeOutEvents.push({
+            type: "TimeOut",
+            hour: parseInt(hour, 10),
+            date
+        });
+        return employee;
+    } catch(e) {
+        throw new Error("Invalid dateStamp provided");
+    }
+}
+
+function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(e => e.date === date);
+    const timeOut = employee.timeOutEvents.find(e => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+
+function allWagesFor(employee) {
+    return employee.timeInEvents.reduce((total, e) => 
+        total + wagesEarnedOnDate(employee, e.date), 0);
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => 
+        employee.firstName === firstNameString);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((total, emp) => 
+        total + allWagesFor(emp), 0);
+}
